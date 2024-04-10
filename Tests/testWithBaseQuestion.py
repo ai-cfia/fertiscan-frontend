095b7663cc134ffbfe8ade597503a62683c8d45f
samuel Pelletier-Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-04-10 09:37:24 -0400

    issue #8: Fixing tests issue.

---
+++ Tests/testWithBaseQuestion.py
@@ -0,0 +1,21 @@
+import unittest
+
+class testWithBaseQuestion(unittest.TestCase):
+    def setUp(self):
+        # This method will be called before each test
+        pass
+
+    def tearDown(self):
+        # This method will be called after each test
+        pass
+
+    def test_case_1(self):
+        # Write your test case here
+        pass
+
+    def test_case_2(self):
+        # Write your test case here
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
