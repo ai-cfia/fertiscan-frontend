fa33f2481f901b1e9b67c2f55233db37366336be
Samuel Pelletier Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-05-30 14:32:16 +0000

    let jules take a look at the code

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,19 @@ import JsonPage from "./Pages/JsonPage/Json"
 import NoPage from "./Pages/NoPage/NoPage";
 import "./App.css";
 import Header from "./Components/Header/Header";
-import { I18nextProvider } from "react-i18next";
-import i18n from "i18next";
 
 function App() {
   return (
-    <I18nextProvider i18n={i18n}>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/">
-          <Route index element={<HomePage />} />
-          <Route path="Json" element={<JsonPage/>}/>
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </I18nextProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePage />} />
+            <Route path="Json" element={<JsonPage/>}/>
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n.js"; // Import the i18n object from the correct module file
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <I18nextProvider i18n={i18n}> // Add the i18n prop to the I18nextProvider component
+    <React.StrictMode>
       <App />
-  </React.StrictMode>,
+    </React.StrictMode>
+  </I18nextProvider> 
 );
