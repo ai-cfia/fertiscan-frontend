13e2137930959ef378dca46b1a42f900d6dd69c1
JulesB <62644404+Youllou@users.noreply.github.com>
2024-05-30 15:59:56 +0000

    fixed upload

--- src/Pages/JsonPage/Json.tsx
+++ src/Pages/JsonPage/Json.tsx
@@ -8,23 +8,25 @@ function JsonPage(){
     const [form, setForm]=useState({})
     const [fetchError, setError]=useState<Error|null>(null)
     const location = useLocation();
-    const files:File[] = location.state.data[0];
+    const files:File[] = location.state.data;
     const [uploadStarted, startUpload] = useState(false);
 
     
-    const api_url = "http://localhost:5000"
+    const api_url = "https://shiny-goggles-75q6p5xj4wwfp6gg-5000.app.github.dev/"
     const upload_all = async ()=>{
+        const res = []
         for(let i=0; i<files.length; i++){
             const formData = new FormData()
             formData.append("file", files[i])
-            await fetch(api_url+"/upload",{
+            res.push(await fetch(api_url+"/upload",{
                 method:'POST',
                 headers:{
                     // if needed, add headers here
                 }, 
                 body:formData
-            })
+            }))
         }
+        return res
     }
     useEffect(()=>{
         if(!uploadStarted){
