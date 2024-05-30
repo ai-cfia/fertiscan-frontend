f22961b06ba2ab6b1d5c2da2316b0fe551380301
ThomasCardin <tomcardin@outlook.com>
2024-05-30 13:13:37 -0400

    issue #26: fixed merge conflict

--- src/Pages/JsonPage/Json.tsx
+++ src/Pages/JsonPage/Json.tsx
@@ -2,31 +2,31 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./Json.css";
 
-function JsonPage(){
-    const [loading, setLoading] = useState(true)
-    const [form, setForm]=useState({})
-    const [fetchError, setError]=useState<Error|null>(null)
-    const location = useLocation();
-    const files:File[] = location.state.data;
-    const [uploadStarted, startUpload] = useState(false);
+function JsonPage() {
+  const [loading, setLoading] = useState(true);
+  const [form, setForm] = useState({});
+  const [fetchError, setError] = useState<Error | null>(null);
+  const location = useLocation();
+  const files: File[] = location.state.data;
+  const [uploadStarted, startUpload] = useState(false);
 
-    
-    const api_url = "https://shiny-goggles-75q6p5xj4wwfp6gg-5000.app.github.dev/"
-    const upload_all = async ()=>{
-        const res = []
-        for(let i=0; i<files.length; i++){
-            const formData = new FormData()
-            formData.append("file", files[i])
-            res.push(await fetch(api_url+"/upload",{
-                method:'POST',
-                headers:{
-                    // if needed, add headers here
-                }, 
-                body:formData
-            }))
-        }
-        return res
+  const api_url = "https://shiny-goggles-75q6p5xj4wwfp6gg-5000.app.github.dev/";
+  const upload_all = async () => {
+    const res = [];
+    for (let i = 0; i < files.length; i++) {
+      const formData = new FormData();
+      formData.append("file", files[i]);
+      res.push(
+        await fetch(api_url + "/upload", {
+          method: "POST",
+          headers: {
+            // if needed, add headers here
+          },
+          body: formData,
+        }),
+      );
     }
+    return res;
   };
   useEffect(() => {
     if (!uploadStarted) {
