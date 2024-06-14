d2fa4984f6d554be9b73828e3487a09a96e0e165
Youllou <juless.barbier@gmail.com>
2024-06-14 11:51:11 -0400

    got rid of ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import dotenv from "dotenv";
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react() ],
   define: {
     "process.env": process.env,
   },
