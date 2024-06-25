fe4d0f72fd0da21552471c475a0c936f983fceef
Samuel Pelletier Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-06-25 17:10:19 +0000

    Add language button that store language localy

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ import NoPage from "./Pages/NoPage/NoPage";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import FormPage from "./Pages/FormPage/FormPage";
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import ConfirmPage from "./Pages/ConfirmPage/ConfirmPage";
 import SideMenu from "./Components/SideMenu/SideMenu";
+import SettingPage from "./Pages/SettingPage/SettingPage";
+import i18next from "i18next";
 
 function App() {
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem('language');
+    i18next.changeLanguage(storedLanguage || 'en');
+  }, []);
+
   return (
     <BrowserRouter>
       <StrictMode>
@@ -23,6 +30,7 @@ function App() {
           <Route path="Json" element={<JsonPage />} />
           <Route path="Form" element={<FormPage />} />
           <Route path="Confirm" element={<ConfirmPage />} />
+          <Route path="Settings" element={<SettingPage />} />
 
           <Route path="*" element={<NoPage />} />
         </Route>
