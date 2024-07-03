44c777b0053f3cc367c71a10f4927404474f6857
Samuel Pelletier Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-07-03 13:48:21 +0000

    Add wrapping to picture name Add top margin to fit the drag and drop component.

--- src/Components/FileList/FileElement/FileElement.tsx
+++ src/Components/FileList/FileElement/FileElement.tsx
@@ -16,9 +16,34 @@ const FileElement: React.FC<FileElementProps> = ({
 }) => {
   const [fileUrl, setFileUrl] = useState("");
   const fileCard = useRef<null | HTMLDivElement>(null);
+  const titleRef = useRef<null | HTMLParagraphElement>(null);
+
   useEffect(() => {
     setFileUrl(blob.blob);
   }, [blob]);
+
+  // This useEffect will adjust the font size of the title to fit the container
+  useEffect(() => {
+    const adjustFontSize = () => {
+      if (titleRef.current) {
+        const maxWidth = fileCard.current ? fileCard.current.offsetWidth : 0;
+        let fontSize = parseInt(
+          window.getComputedStyle(titleRef.current).fontSize,
+          10,
+        );
+
+        // Reduce font size till text fits the container or the font size reaches minimum threshold
+        while (titleRef.current.scrollWidth > maxWidth && fontSize > 10) {
+          fontSize--;
+          titleRef.current.style.fontSize = `${fontSize}px`;
+        }
+      }
+    };
+
+    adjustFontSize();
+    window.addEventListener("resize", adjustFontSize);
+    return () => window.removeEventListener("resize", adjustFontSize);
+  }, []);
 
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault(); // Prevent default navigation
@@ -53,7 +78,13 @@ const FileElement: React.FC<FileElementProps> = ({
       onClick={handleClick}
     >
       <img src={fileUrl} alt={blob.name} />
-      <p className="file-title black bold unselectable">{blob.name}</p>
+      <p
+        ref={titleRef}
+        className="file-title black bold unselectable"
+        style={{ fontSize: "inherit" }}
+      >
+        {blob.name}
+      </p>
       <div className="cross" onClick={deleteImage}></div>
     </div>
   );
