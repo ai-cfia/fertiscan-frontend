c9e7b2a88c08e17e5383750815d261fe1c16efea
Youllou <juless.barbier@gmail.com>
2024-07-10 12:25:16 -0400

    - ConfirmPage handles complex data - ProgressBar follows state after refresh - Complex data is disabled on validation - Css tweaks
    Co-authored-by: Samuel Pelletier Evraire <SamuelPelletierEvraire@users.noreply.github.com>

--- src/Pages/ConfirmPage/ConfirmPage.tsx
+++ src/Pages/ConfirmPage/ConfirmPage.tsx
@@ -1,14 +1,78 @@
 import Carousel from "../../Components/Carousel/Carousel";
 import Section from "../../Model/Section-Model.tsx";
+import Input from "../../Model/Input-Model.tsx";
 import "./ConfirmPage.css";
 import { useTranslation } from "react-i18next";
 import { useContext } from "react";
-import { SessionContext } from "../../Utils/SessionContext";
+import { SessionContext, SetSessionContext } from "../../Utils/SessionContext";
 
 const ConfirmPage = () => {
   const { t } = useTranslation();
   const { state } = useContext(SessionContext);
+  const { setState } = useContext(SetSessionContext);
   const data = state.data.form;
+
+
+
+
+  const renderListInput = (inputInfo : Input) => {
+    return(
+      <ul>
+        {inputInfo.value.map((value,index) => (
+          <li key={index}>{value as string}</li>
+        ))}
+      </ul>
+    )
+  }
+
+  const renderObjectInput = (inputInfo : Input) => {
+    const keys = Object.keys((inputInfo.value as {}[])[0]);
+    return (
+      <div id={inputInfo.id} className="object-input">
+        <table>
+            <colgroup>
+              <col span={1} style={{width:"40%"}}/>
+              <col span={1} style={{width: "20%"}}/>
+              <col span={1} style={{width: "15%"}}/>
+            </colgroup>
+          <thead>
+              {keys.map((key, index) => {
+                return <th key={index}>{key}</th>;
+              })}
+          </thead>
+          <tbody>
+            {inputInfo.value.map((obj, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <p>{(inputInfo.value[index] as {[key:string]:string})[keys[0]]}</p>
+                  </td>
+                  <td>
+                    <p>{(inputInfo.value[index] as {[key:string]:string})[keys[1]]}</p>
+                  </td>
+                  <td>
+                    <p>{(inputInfo.value[index] as {[key:string]:string})[keys[2]]}</p>
+                      
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+
+  const renderInput = (inputInfo : Input) =>{
+    if(inputInfo.isAlreadyTable){
+      return renderListInput(inputInfo);
+    }else if(inputInfo.isInputObjectList){
+      return renderObjectInput(inputInfo);
+    }else{
+      return inputInfo.value as unknown as string;
+    }
+  }
 
   // Traduction not done waiting on prompt changes
   const renderSection = (section: Section) => (
@@ -17,12 +81,16 @@ const ConfirmPage = () => {
       <ul className="data-infos">
         {section.inputs.map((input) => (
           <li key={input.id}>
-            <b>{input.label}:</b> {input.value}
+            <b>{input.label}:</b> {renderInput(input)}
           </li>
         ))}
       </ul>
     </div>
   );
+
+  const cancel = () => {
+    setState({ ...state, state: "form" });
+  };
 
   return (
     <div className="confirm-page-container ${theme}">
@@ -36,7 +104,7 @@ const ConfirmPage = () => {
       <div className="confirm-container">
         {data.sections.map((section: Section) => renderSection(section))}
         <div className="button-container">
-          <button onClick={() => console.log("Cancel")}>
+          <button onClick={() => cancel()}>
             {t("cancelButton")}
           </button>
           <button onClick={() => console.log("Confirm")}>
--- src/Utils/EventChannels.tsx
+++ src/Utils/EventChannels.tsx
@@ -5,6 +5,7 @@ export const FormClickActions = eventbus<{
   ModifyClick: (inputInfo: Input) => void;
   ApproveClick: (inputInfo: Input) => void;
   Rejected: (inputInfo: Input) => void;
+  SyncProgress: (inputInfo: Input) => void;
 }>();
 
 export const MenuChannel = eventbus<{
