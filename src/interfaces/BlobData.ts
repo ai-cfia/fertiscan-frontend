a7fcfd7a8387290346d31abc4eedc2668764d435
K. Allagbe <kotchikpa.allagbe@gmail.com>
2024-07-15 09:53:18 -0400

    issue #123: some doc strings

--- src/interfaces/BlobData.ts
+++ src/interfaces/BlobData.ts
@@ -1,6 +1,13 @@
+/**
+ * Interface representing the data for a selected image blob.
+ * This is used to hold selected images inside the global state object.
+ */
 interface BlobData {
+  /** The base64 or binary string representation of the image blob. */
   blob: string;
+  /** The name of the image file. */
   name: string;
+  /** The size of the image file in bytes. */
   size: number;
 }
 
--- src/interfaces/StateType.ts
+++ src/interfaces/StateType.ts
@@ -1,10 +1,17 @@
 import Data from "../Model/Data-Model";
 import BlobData from "./BlobData";
 
+/**
+ * Interface representing the global state of the application.
+ * This object is saved in local storage.
+ */
 interface StateType {
+  /** The current state or phase of the application. */
   state: string;
   data: {
+    /** An array of BlobData objects representing selected images. */
     pics: BlobData[];
+    /** The fertilizer inspection form data associated with the current state. */
     form: Data;
   };
 }
