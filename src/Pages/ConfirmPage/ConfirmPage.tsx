b74e016d18d441df6e5d84ac57da3f3c1ad79f01
Samuelpelletier3124 <samuelpelleva6@gmail.com>
2024-07-16 16:02:45 +0000

    - Add message and traduction - Add checkbox confirmation

--- src/Pages/ConfirmPage/ConfirmPage.tsx
+++ src/Pages/ConfirmPage/ConfirmPage.tsx
@@ -3,7 +3,7 @@ import Section from "../../Model/Section-Model.tsx";
 import Input from "../../Model/Input-Model.tsx";
 import "./ConfirmPage.css";
 import { useTranslation } from "react-i18next";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SessionContext, SetSessionContext } from "../../Utils/SessionContext";
 
 const ConfirmPage = () => {
@@ -106,6 +106,18 @@ const ConfirmPage = () => {
     </div>
   );
 
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (event: { target: { checked: any; }; }) => {
+      // Actualiser l'état avec la nouvelle valeur de la case à cocher
+      setIsChecked(event.target.checked);
+      if (event.target.checked) {
+          console.log('Checkbox is checked.');
+      } else {
+          console.log('Checkbox is not checked.');
+      }
+  };
+
   return (
     <div className="confirm-page-container ${theme}">
       <h1 id="confirm-title">{t("confirmationPage")}</h1>
@@ -118,6 +130,18 @@ const ConfirmPage = () => {
       />
       <div className="confirm-container">
         {data.sections.map((section: Section) => renderSection(section))}
+        <div className="checkbox-container">
+        <input
+          id="confirmation-checkbox"
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleChange}
+          className="checkbox-input"
+        />
+        <label htmlFor="confirmation-checkbox" className="checkbox-label">
+          {t("confirmationCheckbox")}
+        </label>
+      </div>
         <div className="button-container">
           <button onClick={() => cancel()}>{t("cancelButton")}</button>
           <button onClick={() => console.log("Confirm")}>
