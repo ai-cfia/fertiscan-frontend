0ef9762795ae272fd33f370e61e2e08041a9ff6e
Youllou <juless.barbier@gmail.com>
2024-07-22 14:26:43 -0400

    lint fix

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -378,11 +378,13 @@ const InputComponent: React.FC<InputProps> = ({
 
   useEffect(() => {
     FormClickActions.emit("SyncProgress", inputInfo);
-    if(inputInfo.isAlreadyTable) {
-      document.querySelectorAll(`#${inputInfo.id} textarea`).forEach((textarea) => {
-        resizeTextarea(textarea as HTMLElement);
-      });
-    }else{
+    if (inputInfo.isAlreadyTable) {
+      document
+        .querySelectorAll(`#${inputInfo.id} textarea`)
+        .forEach((textarea) => {
+          resizeTextarea(textarea as HTMLElement);
+        });
+    } else {
       resizeTextarea(textarea.ref.current);
     }
     // eslint-disable-next-line
