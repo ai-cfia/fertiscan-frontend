0852789413c36b1296329aea55d091ec255fae27
Samuel Pelletier Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-07-25 15:44:04 +0000

    push before switching share to jules

--- src/Components/SavedLabelCard/SavedLabelCard.tsx
+++ src/Components/SavedLabelCard/SavedLabelCard.tsx
@@ -46,14 +46,23 @@ const SavedLabelCard: React.FC<SavedLabelCardProps> = ({
       ref={fileCard}
       className="card-test"
       onClick={handleClick()}>
+      <div className="company-grid">
         <p className="company-label">{t("Company Name")}</p>
         <p className="company-result">/Company Name/</p>
+      </div>
+      <div className="fertilizer-grid">
         <p className="fertilizer-label">{t("fertilizer Name")}</p>
         <p className="fertilizer-result">/Fertilizer Name/</p>
+      </div>
+      <div className="registration-grid">
         <p className="registration-label">{t("registration Number")}</p>
         <p className="registration-result">/Registration Number/</p>
+      </div>
+      <div className="lot-number-grid">
         <p className="lot-number-label">{t("lot Number")}</p>
         <p className="lot-number-result">/Lot Number/</p>
+      </div>
+      <div className="footer-grid">
         <div className="user-grid">
           <p className="user-label">{t("User")}</p>
           <p className="user-result">/User/</p>
@@ -61,7 +70,8 @@ const SavedLabelCard: React.FC<SavedLabelCardProps> = ({
         <div className="upload-grid">
           <p className="date-label">{t("Date")}</p>
           <p className="date-result">/Date/</p>
-          </div>
+        </div>
+      </div>
     </div>
   );
 };
