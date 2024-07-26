37b70768c7cf10538a4eb8597ec15a2fda42167e
Samuel Pelletier Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-07-26 16:59:10 +0000

    fix design and positioning of saved label

--- src/Pages/SavedListPage/SavedListPage.tsx
+++ src/Pages/SavedListPage/SavedListPage.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./SavedListPage.css";
 import "./Pagination.css"
 import SavedLabelCard from "../../Components/SavedLabelCard/SavedLabelCard";
 import ReactPaginate from 'react-paginate';
 
 const SavedListPage = () => {
+
+    const listContainer = useRef<HTMLDivElement>(null);
+    
     // Constant for the number of labels per page
     const labelsPerPage = 5;
     // State for the list of labels and the current page
@@ -20,7 +23,15 @@ const SavedListPage = () => {
     // Function to handle page change
     const handlePageClick = ({ selected }: { selected: number }) => {
         setCurrentPage(selected);
+        scrollToTop();
     };
+
+    const scrollToTop = () => {
+        if (listContainer.current) {
+          listContainer.current.scrollTo({ top: 0, behavior: "smooth" });
+        }
+      };
+
     // Effect for fetching labels data
     useEffect(() => {
         if (process.env.REACT_APP_ACTIVATE_USING_JSON == "true") {
@@ -33,28 +44,36 @@ const SavedListPage = () => {
     }, [])
 
     return (
-          <div className="saved-label-list">
+        <div className="saved-label-list" ref={listContainer}>
             {/* Display the current labels */}
             {currentLabels.map((label, index) => (
-              <SavedLabelCard key={index} label={label} />
+                <SavedLabelCard key={index} label={label} />
             ))}
             {/* Pagination Container */}
             <div className="pagination-container">
-              <ReactPaginate
-                previousLabel={'<'}
-                nextLabel={'>'}
-                breakLabel={'...'}
-                breakClassName={'break-me'}
-                pageCount={pageCount}
-                marginPagesDisplayed={2}
-                pageRangeDisplayed={5}
-                onPageChange={handlePageClick}
-                containerClassName={'pagination'}
-                activeClassName={'active'}
-              />
+                <ReactPaginate
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item previous"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item next"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    renderOnZeroPageCount={null}
+                />
             </div>
-          </div>
-      );
+        </div>
+    );
 };
 
 export default SavedListPage;
