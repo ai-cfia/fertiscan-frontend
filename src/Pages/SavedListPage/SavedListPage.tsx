f2c3379f13a318e3e557d0a4b56c2aca82dcd915
Samuel Pelletier Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-07-26 15:20:06 +0000

    Added Pagination component new package npm -Modify next and prev button class from carousel to .next-image .prev-image because of the conflic class -Add pagination to the list of saved Label

--- src/Pages/SavedListPage/SavedListPage.tsx
+++ src/Pages/SavedListPage/SavedListPage.tsx
@@ -1,30 +1,60 @@
-//import { useTranslation } from "react-i18next";
-import { useEffect, useRef, useState } from "react";
-import "./SavedListPage.css"; 
+import { useEffect, useState } from "react";
+import "./SavedListPage.css";
+import "./Pagination.css"
 import SavedLabelCard from "../../Components/SavedLabelCard/SavedLabelCard";
+import ReactPaginate from 'react-paginate';
 
 const SavedListPage = () => {
-    // For local development
-    //const api_url = "http://localhost:5000";
-    //const { t } = useTranslation();
-
+    // Constant for the number of labels per page
+    const labelsPerPage = 5;
+    // State for the list of labels and the current page
     const [labels, setLabels] = useState([]);
-            
-    useEffect(()=>{
-        if(process.env.REACT_APP_ACTIVATE_USING_JSON=="true"){
+    const [currentPage, setCurrentPage] = useState(0);
+    // Calculate the total number of pages
+    const pageCount = Math.ceil(labels.length / labelsPerPage);
+    // Calculate the labels to be displayed on the current page
+    const currentLabels = labels.slice(
+        currentPage * labelsPerPage,
+        (currentPage + 1) * labelsPerPage
+    );
+    // Function to handle page change
+    const handlePageClick = ({ selected }: { selected: number }) => {
+        setCurrentPage(selected);
+    };
+    // Effect for fetching labels data
+    useEffect(() => {
+        if (process.env.REACT_APP_ACTIVATE_USING_JSON == "true") {
             fetch("/label_list.json")
-            .then(response => response.json())
-            .then(data => {
-                setLabels(data);
-            });
+                .then(response => response.json())
+                .then(data => {
+                    setLabels(data);
+                });
         }
-    },[])
-return (
-    <div className="saved-label-list">
-        {labels.map((label, index) => (
-            <SavedLabelCard key={index} label={label} />
-        ))}
-    </div>
-);
-}
+    }, [])
+
+    return (
+          <div className="saved-label-list">
+            {/* Display the current labels */}
+            {currentLabels.map((label, index) => (
+              <SavedLabelCard key={index} label={label} />
+            ))}
+            {/* Pagination Container */}
+            <div className="pagination-container">
+              <ReactPaginate
+                previousLabel={'<'}
+                nextLabel={'>'}
+                breakLabel={'...'}
+                breakClassName={'break-me'}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={'pagination'}
+                activeClassName={'active'}
+              />
+            </div>
+          </div>
+      );
+};
+
 export default SavedListPage;
