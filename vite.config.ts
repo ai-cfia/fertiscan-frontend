6c8255e199b0e05e2546cd06c4b99dba2b6944de
Youllou <juless.barbier@gmail.com>
2024-08-28 17:25:25 -0400

    lint fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig,loadEnv } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dotenv from "dotenv";
 dotenv.config();
 // https://vitejs.dev/config/
 // eslint-disable-next-line
 // @ts-ignore
-export default ({mode})=> defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env": {...process.env, ...loadEnv(mode, process.cwd())},
-  },
-});
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [react()],
+    define: {
+      "process.env": { ...process.env, ...loadEnv(mode, process.cwd()) },
+    },
+  });
