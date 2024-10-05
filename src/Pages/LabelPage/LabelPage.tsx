631f7a3f052465760596c4d3d313a91dd5fe9627
K. Allagbe <kotchikpa.allagbe@gmail.com>
2024-10-05 08:06:42 -0400

    issue #259: fix label display

--- src/Pages/LabelPage/LabelPage.tsx
+++ src/Pages/LabelPage/LabelPage.tsx
@@ -1,7 +1,12 @@
+import axios from "axios";
+import merge from "deepmerge";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Label from "../../Components/Label/Label";
+import { createDefaultInspection } from "../../interfaces/Inspection";
 import Data from "../../Model/Data-Model";
+import { useAlert } from "../../Utils/AlertContext";
+import { combineMerge } from "../../Utils/deepMerge";
 import { FertiliserForm, populateFromJSON } from "../../Utils/FormCreator";
 import "./LabelPage.css";
 
@@ -9,6 +14,7 @@ function LabelPage() {
   const params = useParams();
   const labelId = params.labelId;
   const [label, setLabel] = useState<Data>(FertiliserForm());
+  const { showAlert } = useAlert();
   useEffect(() => {
     if (process.env.REACT_APP_ACTIVATE_USING_JSON == "true") {
       fetch("/debug/" + labelId + ".json")
@@ -17,16 +23,31 @@ function LabelPage() {
           setLabel(populateFromJSON(label, data));
         });
     } else {
-      fetch(process.env.VITE_API_URL + "/inspections/" + labelId, {
-        headers: {
-          Authorization:
-            "Basic " + document.cookie.split("auth=")[1].split(";")[0],
-        },
-      })
-        .then((r) => r.json())
-        .then((data) => {
-          data = JSON.parse(data);
-          setLabel(populateFromJSON(label, data));
+      axios
+        .get(process.env.VITE_API_URL + "/inspections/" + labelId, {
+          headers: {
+            Authorization:
+              "Basic " + document.cookie.split("auth=")[1].split(";")[0],
+          },
+        })
+        .then((response) => {
+          let data = response.data;
+
+          if (typeof data === "string") {
+            try {
+              data = JSON.parse(data);
+            } catch (e) {
+              console.error("Error parsing JSON string:", e);
+            }
+          }
+          const inspection = merge(createDefaultInspection(), data, {
+            arrayMerge: combineMerge,
+          });
+          setLabel(populateFromJSON(label, inspection));
+        })
+        .catch((error) => {
+          console.error("Error fetching the inspection:", error);
+          showAlert(String(error), "error");
         });
     }
     // eslint-disable-next-line
