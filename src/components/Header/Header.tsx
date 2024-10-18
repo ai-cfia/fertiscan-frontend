52e131a42cdfdaffb4a1aa15ab3591dbf2041a48
Samuel Pelletier Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-10-18 15:23:09 +0000

    remove almost all use of the css file but keep logo because it is a bug with the Image component of next.js

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,20 +12,21 @@ const Header: React.FC = () => {
             <AppBar position="static">
                 <Toolbar>
 
-                    <IconButton color={"inherit"} edge="start" aria-label="menu" sx={{position:'relative'}}>
+                    <IconButton color={"inherit"} edge="start" aria-label="menu" >
                         <MenuIcon sx={{fontSize: {xs: '5vw',md: '3vw', xl:'3vw'}}}/>
                     </IconButton>
 
-                    <Box sx={{ width: {xs: '100%'}, display:'flex', justifyContent:'center'}}>
-                        <Box className={styles.logo} sx={{ width: {xs: '25vw'}, height:'100%'}}>
+                    <Box position={'relative'} sx={{ width: {xs: '100%'}, display:'flex', justifyContent:'center'}}>
+                        <Box  sx={{ width: {xs: '25vw'}, height:'100%'}}>
                         <Link href={"https://inspection.canada.ca"}>
+                        {/**Neew to use a classname here because it is a workaround to a bug**/ }
                             <Image className={styles.logo} src={"/img/CFIA FIP FR WHITE 1.png"} alt="logo" layout="fill" objectPosition='50% 50%'  />
                         </Link>
                         </Box>
                     </Box>
                     <Box sx={{ display: 'flex', alignItems: 'end' }}>
-                        <Button color={"inherit"} className={styles.languageButton} sx={{ padding: {xs: '0.1vw',md: '0.5vw', lg:'0.5vw', xl:'0.5vw'}, display:'contents'}}>
-                            <Typography sx={{alignSelf:'center', fontSize: {xs: '2vw',md: '1.5vw', lg:'1vw', xl:'1.2vw'},}}>Français</Typography>
+                        <Button color={"inherit"} sx={{ padding: {xs: '0.1vw',md: '0.5vw', lg:'0.5vw', xl:'0.5vw'}, display:'contents', textTransform:'unset'}}>
+                            <Typography sx={{alignSelf:'center', fontSize: {xs: '2vw',md: '1.5vw', lg:'1vw', xl:'1.2vw'}, textDecoration:'underline'}}>Français</Typography>
                         </Button>
                         <IconButton color={"inherit"}>
                             <AccountCircleIcon fontSize={"large"} sx={{fontSize: {xs: '3vw',md: '3vw', xl:'2.5vw'}}}/>
