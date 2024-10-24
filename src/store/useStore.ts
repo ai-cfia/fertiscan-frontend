64b9574646a5a3cac0fdadbd239862448a0f7cfd
samuel Pelletier-Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-10-24 14:56:25 -0400

    - removing zustand use for SideNavOpen

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,17 +1,5 @@
 import { create } from "zustand";
 
-interface State {
-  // Represents whether the side navigation is open or closed
-  sideNavOpen: boolean;
+interface State {}
 
-  // Function to set the state of 'sideNavOpen'
-  setSideNavOpen: (open: boolean) => void;
-}
-
-export const useStore = create<State>((set) => ({
-  // Initial state for side navigation
-  sideNavOpen: false,
-
-  // Action to set the 'sideNavOpen' state
-  setSideNavOpen: (open) => set({ sideNavOpen: open }),
-}));
+export const useStore = create<State>((set) => ({}));
