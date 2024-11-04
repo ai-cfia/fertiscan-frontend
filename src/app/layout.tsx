19757d78fc0154ddc7adce49addd3b664522721a
samuel Pelletier-Evraire <samuel.pelletier-evraire@inspection.gc.ca>
2024-11-04 13:36:10 -0500

    Configurating base side-nav (No animation, over the other component, close when clicking outside)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,17 +13,26 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   const [sideNavOpen, setSideNavOpen] = useState(false);
 
+  const handleDrawerToggle = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'esc')) {
+      return;
+    }
+    setSideNavOpen(open);
+  };
+
+  const handleDrawerClose = () => {
+    setSideNavOpen(false);
+  };
+
   return (
     <html lang="en">
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
-            <SideNav open={sideNavOpen} />
+            <SideNav open={sideNavOpen} onClose={handleDrawerClose} />
             {/* Margin adjustment based on SideNav state */}
-            <Box
-              marginLeft={sideNavOpen ? "240px" : 0}
-            >
-              <Header setSideNavOpen={setSideNavOpen}/>
+            <Box>
+              <Header setSideNavOpen={setSideNavOpen} />
               {children}
             </Box>
           </ThemeProvider>
