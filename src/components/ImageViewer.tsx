b681237e969f6d77b90e7d7614a77f4bc970f300
K. Allagbe <kotchikpa.allagbe@gmail.com>
2024-11-22 15:32:38 -0500

    issue #324: fix a tooltip warning

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -64,8 +64,8 @@ const ImageViewer: React.FC<ImageViewerProps> = ({ imageFiles }) => {
         onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
         noSwiping
         noSwipingClass="no-swipe"
-        pagination={{ clickable: true }} // Enable pagination
-        modules={[Pagination]} // Register the Pagination module
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
       >
         {imageUrls.map((url, index) => (
           <SwiperSlide key={index} data-testid={`slide-${index + 1}`}>
@@ -131,55 +131,68 @@ const ControlBar: React.FC<ControlBarProps> = ({
       className="flex items-center justify-center gap-4 p-4 flex-wrap"
       data-testid="control-bar"
     >
-      <Tooltip title="Previous">
-        <Button
-          variant="contained"
-          onClick={() => swiper?.slidePrev()}
-          data-testid="prev-button"
-          disabled={activeIndex <= 0}
-        >
-          <ArrowBackIosIcon />
-        </Button>
+      <Tooltip title="Previous" disableHoverListener={activeIndex <= 0}>
+        <span>
+          <Button
+            variant="contained"
+            onClick={() => swiper?.slidePrev()}
+            data-testid="prev-button"
+            disabled={activeIndex <= 0}
+          >
+            <ArrowBackIosIcon />
+          </Button>
+        </span>
       </Tooltip>
-      <Tooltip title="Next">
-        <Button
-          variant="contained"
-          onClick={() => swiper?.slideNext()}
-          data-testid="next-button"
-          disabled={activeIndex >= zoomRefs.length - 1}
-        >
-          <ArrowForwardIosIcon />
-        </Button>
+      <Tooltip
+        title="Next"
+        disableHoverListener={activeIndex >= zoomRefs.length - 1}
+      >
+        <span>
+          <Button
+            variant="contained"
+            onClick={() => swiper?.slideNext()}
+            data-testid="next-button"
+            disabled={activeIndex >= zoomRefs.length - 1}
+          >
+            <ArrowForwardIosIcon />
+          </Button>
+        </span>
       </Tooltip>
-      <Tooltip title="Zoom In">
-        <Button
-          variant="contained"
-          onClick={() => currentZoomRef?.zoomIn()}
-          data-testid="zoom-in-button"
-          disabled={!currentZoomRef}
-        >
-          <ZoomInIcon />
-        </Button>
+      <Tooltip title="Zoom In" disableHoverListener={!currentZoomRef}>
+        <span>
+          <Button
+            variant="contained"
+            onClick={() => currentZoomRef?.zoomIn()}
+            data-testid="zoom-in-button"
+            disabled={!currentZoomRef}
+          >
+            <ZoomInIcon />
+          </Button>
+        </span>
       </Tooltip>
-      <Tooltip title="Zoom Out">
-        <Button
-          variant="contained"
-          onClick={() => currentZoomRef?.zoomOut()}
-          data-testid="zoom-out-button"
-          disabled={!currentZoomRef}
-        >
-          <ZoomOutIcon />
-        </Button>
+      <Tooltip title="Zoom Out" disableHoverListener={!currentZoomRef}>
+        <span>
+          <Button
+            variant="contained"
+            onClick={() => currentZoomRef?.zoomOut()}
+            data-testid="zoom-out-button"
+            disabled={!currentZoomRef}
+          >
+            <ZoomOutIcon />
+          </Button>
+        </span>
       </Tooltip>
-      <Tooltip title="Reset zoom">
-        <Button
-          variant="contained"
-          onClick={() => zoomRefs.forEach((ref) => ref?.resetTransform())}
-          data-testid="reset-button"
-          disabled={!currentZoomRef}
-        >
-          <YoutubeSearchedForIcon />
-        </Button>
+      <Tooltip title="Reset zoom" disableHoverListener={!currentZoomRef}>
+        <span>
+          <Button
+            variant="contained"
+            onClick={() => zoomRefs.forEach((ref) => ref?.resetTransform())}
+            data-testid="reset-button"
+            disabled={!currentZoomRef}
+          >
+            <YoutubeSearchedForIcon />
+          </Button>
+        </span>
       </Tooltip>
     </Box>
   );
