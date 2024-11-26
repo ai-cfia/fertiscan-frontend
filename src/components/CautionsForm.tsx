169cb3bdf09be3ce12c9a28685ace8a0efa902e3
K. Allagbe <kotchikpa.allagbe@gmail.com>
2024-11-25 20:42:06 -0500

    issue #326: cautions form

--- src/components/CautionsForm.tsx
+++ src/components/CautionsForm.tsx
@@ -1,11 +1,36 @@
-import { FormComponentProps } from "@/types/types";
+import { FormComponentProps, LabelData } from "@/types/types";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
+import { FormProvider, useForm, useWatch } from "react-hook-form";
+import VerifiedBilingualTable from "./VerifiedBilingualTable";
 
 function CautionsForm({ labelData, setLabelData }: FormComponentProps) {
+  const methods = useForm<LabelData>({
+    defaultValues: labelData,
+  });
+
+  const { control } = methods;
+
+  const watchedCautions = useWatch({
+    control,
+    name: "cautions",
+  });
+
+  useEffect(() => {
+    if (watchedCautions) {
+      setLabelData((prevLabelData) => ({
+        ...prevLabelData,
+        cautions: watchedCautions,
+      }));
+    }
+  }, [watchedCautions, setLabelData]);
+
   return (
-    <Box className="p-4" data-testid="cautions-form">
-      CautionsForm
-    </Box>
+    <FormProvider {...methods}>
+      <Box className="p-4" data-testid="cautions-form">
+        <VerifiedBilingualTable path={"cautions"} />
+      </Box>
+    </FormProvider>
   );
 }
 
